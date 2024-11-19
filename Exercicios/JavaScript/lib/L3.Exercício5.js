cd2f2cfd3d9a52603fce842028f1d66a9689c806
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-19 16:01:50 -0300

    Resoluçao do exercicio 5 da lista 3

---
+++ Exercicios/JavaScript/lib/L3.Exercício5.js
@@ -0,0 +1,33 @@
+// function maiorNumero(num1, num2, num3) {
+//     var maior = num1 
+    
+//      if (num2 > maior) {
+//         maior = num2
+//      }
+//      if (num3 > maior) {
+//         maior = num3
+//      }
+
+//      return maior
+ 
+// }
+// console.log(maiorNumero(3, 7, 5))
+// console.log(maiorNumero(-1, -5, -3))
+// console.log(maiorNumero(8, 2, 10))
+
+
+function maiorNumero(num1, num2, num3) {
+
+    if (num2 > num1) {
+       num1 = num2
+    }
+    if (num3 > num1) {
+       num1 = num3
+    }
+
+    return num1
+
+}
+console.log(maiorNumero(3, 7, 5))
+console.log(maiorNumero(-1, -5, -3))
+console.log(maiorNumero(8, 2, 10))
