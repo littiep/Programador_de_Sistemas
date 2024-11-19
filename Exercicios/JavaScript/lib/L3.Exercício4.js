8dead9b08b634c121f370b71390a4fc7e95f534d
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-19 16:00:58 -0300

    Resoluçao exercicio 4 da lista 3

---
+++ Exercicios/JavaScript/lib/L3.Exercício4.js
@@ -0,0 +1,16 @@
+function maiorNumero(num1, num2, num3) {
+    var menor = num1 
+
+     if (num2 < menor) {
+        menor = num2
+     }
+     if (num3 < menor) {
+        menor = num3
+     }
+
+     return menor
+ 
+}
+console.log(maiorNumero(3, 7, 5))
+console.log(maiorNumero(-1, -5, -3))
+console.log(maiorNumero(8, 2, 10))
