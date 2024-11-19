95522f7f8c9f2bca2a357d398c73235971be70bd
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-19 16:02:00 -0300

    Resoluçao do exercicio 6 da lista 3

---
+++ Exercicios/JavaScript/lib/L3.Exercício6.js
@@ -0,0 +1,11 @@
+function duplo(x, y) {
+   if (x % y == 0){
+      return true
+   }
+   else {
+      return false
+   }
+}
+console.log(duplo(10, 5))
+console.log(duplo(12, 7))
+console.log(duplo(20, 4))
