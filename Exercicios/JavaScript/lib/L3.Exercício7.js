a72bbd2a4fc2e08d008e97c39bf3883646bed39b
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-19 16:02:07 -0300

    Resoluçao do exercicio 7 da lista 3

---
+++ Exercicios/JavaScript/lib/L3.Exercício7.js
@@ -0,0 +1,14 @@
+function bissexto(ano) {
+   if (ano % 4 == 0 && ano % 100 != 0 || (ano % 400 == 0)) {
+      return "true"
+   }
+   return "false"
+}
+console.log(bissexto(2020))
+console.log(bissexto(2024))
+console.log(bissexto(1999))
+console.log(bissexto(2014))
+console.log(bissexto(2025))
+console.log(bissexto(1900))
+console.log(bissexto(2000))
+console.log(bissexto(2100))
