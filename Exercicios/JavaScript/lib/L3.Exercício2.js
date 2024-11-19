ee762422f89a75f2576ef0e3df50ed136f5fd14b
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-19 16:00:38 -0300

    Resoluçao exercicio 2 da lista 3

---
+++ Exercicios/JavaScript/lib/L3.Exercício2.js
@@ -0,0 +1,13 @@
+function iop(x) {
+    if (x % 2 === 0) {
+        return "Par"
+    }
+    else {
+        return "Impar"
+    }
+} 
+ 
+console.log(iop(4))
+console.log(iop(7))
+console.log(iop(0))
+console.log(iop(-5));
