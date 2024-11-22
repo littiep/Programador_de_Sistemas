2d19776d29365cdfd79ea4b18bfd91a68e63b260
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-22 17:22:22 -0300

    aula 22

---
+++ Exercicios/JavaScript/lib/L3.Exercício12.js
@@ -0,0 +1,21 @@
+// function imprimirTabuada(x) {
+//     var m = 0, p = 0
+//     while (m <= 10) {
+//         p = x * m
+//         console.log(`${x} x ${m} = ${p}`)
+//         m++
+//     }
+//     return "\n"
+// }
+
+
+// imprimirTabuada(2)
+// imprimirTabuada(10)
+
+
+function imprimirTabuada(x) {
+    for (var m = o; m <= 10; m++) {
+        var p = x * m
+        console.log(`${x} x ${m} = ${p}`)
+    }
+}
---
+++ Exercicios/JavaScript/lib/L3.Exercício13.js
@@ -0,0 +1,7 @@
+function imprimirTabuada(x) {
+    for (x = 1; x <= 10; x++) {
+        while (m <= 10) {
+            var p = x * m
+        }
+    }
+}
