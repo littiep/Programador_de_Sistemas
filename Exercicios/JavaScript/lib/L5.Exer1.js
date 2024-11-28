c23b5e78c028ff46850a16876605e5fc71979b01
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-28 17:27:00 -0300

    resoluçao lista 5 exercicio 1

---
+++ Exercicios/JavaScript/lib/L5.Exer1.js
@@ -0,0 +1,9 @@
+// var frase = "O rato roeu a roupa do rei de Roma"
+// const palavras = frase.split(' ')
+// console.log(palavras)
+function separarFrase(frase, separador) {
+    var listaDeFrase = frase.split(separador)
+    console.log(frase)
+}
+
+separarFrase("Oh brave new world that has such people in it.", " ")
--- Exercicios/JavaScript/lib/objeto.js
+++ Exercicios/JavaScript/lib/objeto.js
@@ -1,58 +1,60 @@
-    var carro1 = {
-        marca: "subaru",
-        modelo: "brz",
-        ano : 2015,
-        cor: "azul",
-        traçao: "4x4",
-        aro: 18,
-        motor:"2.2 v4",
-        combustão:"gasolina",
-        carroceria: "simples",
-        cambio:"6 marcha",
-        potencia:300 ,
-        chassi: "547af4g3",
-        calibragem: {
-            dianteiro:20,
-            traseiro:32,
-            step:36,
-        },
-        velocidade:0,
-        acelerar: function() {
-            this.velocidade += 5
-        },
-        frear: function() {
-            this.velocidade  -= 13
-            if (this.velocidade < 0 )  this.velocidade = 0
-        },
-        imprimir: function() {console.table(this)},
-        velocidadeMaxima:220,
-        classe: "esportivo",
-        localDeFabricaçao:"massachussets",
-    }
-    var porsche = {
-        marca: "porsche",
-        modelo: "911 carrera",
-        velocidade: 0,
-        acelerar: function() {
-            this.velocidade += 5
-        },
-        frear: function() {
-            this.velocidade  -= 13
-            if (this.velocidade < 0 )  this.velocidade = 0
-        },
-    }
+var carro1 = {
+    marca: "subaru",
+    modelo: "brz",
+    ano: 2015,
+    cor: "azul",
+    traçao: "4x4",
+    aro: 18,
+    motor: "2.2 v4",
+    combustão: "gasolina",
+    carroceria: "simples",
+    cambio: "6 marcha",
+    potencia: 300,
+    chassi: "547af4g3",
+    calibragem: {
+        dianteiro: 20,
+        traseiro: 32,
+        step: 36,
+    },
+    velocidade: 0,
+    acelerar: function () {
+        this.velocidade += 5
+    },
+    frear: function () {
+        this.velocidade -= 13
+        if (this.velocidade < 0) this.velocidade = 0
+    },
+    imprimir: function () { console.table(this) },
+    velocidadeMaxima: 220,
+    classe: "esportivo",
+    localDeFabricaçao: "massachussets",
+}
+var porsche = {
+    marca: "porsche",
+    modelo: "911 carrera",
+    velocidade: 0,
+    acelerar: function () {
+        this.velocidade += 5
+    },
+    frear: function () {
+        this.velocidade -= 13
+        if (this.velocidade < 0) this.velocidade = 0
+    },
+}
 
-    while (carro1.velocidade < 80) carro1.acelerar()
-
-
-    // while(porsche.velocidade < 80)  porsche.acelerar()
-  
-    // while (porsche.velocidade > 60) porsche.frear()
-
-    // while (porsche.velocidade > 0)  porsche.frear()
+while (carro1.velocidade < 80) carro1.acelerar()
 
 
+// while(porsche.velocidade < 80)  porsche.acelerar()
 
-    // console.table(p(carro1))
-    // console.table(p(carro2))
+// while (porsche.velocidade > 60) porsche.frear()
+
+// while (porsche.velocidade > 0)  porsche.frear()
+
+
+
+// console.table(p(carro1))
+// console.table(p(carro2))
 console.table(carro1)
+
+
