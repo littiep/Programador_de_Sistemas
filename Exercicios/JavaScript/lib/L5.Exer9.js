ba493f32bd03539c1b46ffca255ef73f5b87258b
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-29 16:20:36 -0300

    resoluçao lista 5 Exer9

---
+++ Exercicios/JavaScript/lib/L5.Exer9.js
@@ -0,0 +1,11 @@
+function separarFrase(texto, separador) {
+    var listaDeFrase = texto.split(separador)
+    console.log(listaDeFrase)
+    // return texto.split(separador)
+}
+// ("9. extraia horas, minutos e segundos de um horário:
+console.log("\n----------------------------------------------------")
+console.log("9. extraia horas, minutos e segundos de um horário:", ":")
+
+separarFrase("14:35:20", ":")
+
