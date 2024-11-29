1f22886c427a1fac272701e623ac4d9a95f2dd22
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-11-29 16:20:06 -0300

    resoluçao lista 5 Exer6

---
+++ Exercicios/JavaScript/lib/L5.Exer6.js
@@ -0,0 +1,13 @@
+function separarFrase(texto, separador) {
+    var listaDeFrase = texto.split(separador)
+    console.log(listaDeFrase)
+    // return texto.split(separador)
+}
+
+// ("6. separe uma frase em letras:")
+console.log("\n----------------------------------------------------")
+console.log("6. Separar uma frase em letras\n")
+
+separarFrase("JavaScript", "")
+// separarFrase("Eu gosto de programar", "")
+// separarFrase("Olá, Mundo", "")
