718d880c796075b911b825cf7e34836858e82fc5
littiep <gusatvo.martinsr@gmail.com>
2024-12-02 22:58:33 -0300

    Update bee1011.js

--- Exercicios/BeeCrowd/bee1011.js
+++ Exercicios/BeeCrowd/bee1011.js
@@ -1,1 +1,6 @@
-
+var input = require('fs').readFileSync('./input.txt', 'utf8');
+// console.log(input)
+var lines = input.split('\n');
+// console.log(lines)
+var r = Number(lines[0])
+var res = (4/3) * 3.14159 * (r * 3)
