df91d3fa1bef75aded570426e0376f7baa645a92
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-16 16:57:08 -0300

    refatoraçao

--- Exercicios/JavaScript/lib/L3.Exercício12.js
+++ Exercicios/JavaScript/lib/L3.Exercício12.js
@@ -11,14 +11,18 @@
 // imprimirTabuada(2)
 // imprimirTabuada(10)
 
-function tabuada(x) {
-    for (var m = 0; m <= 10; m++) {
-        var p = x * m
-        console.log(`${x} x ${m} = ${p}`)
-    }
+// function tabuada(x) {
+//     for (var m = 0; m <= 10; m++) {
+//         var p = x * m
+//         console.log(`${x} x ${m} = ${p}`)
+//     }
 
-    return console.log()
+//     return console.log()
 
-}
+// }
+
+import { tabuada } from "../biblio.js"
+
+
 tabuada(2)
 tabuada(10)
