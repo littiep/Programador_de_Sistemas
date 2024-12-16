d12ba2269059ae51a3be4d3f00e840106ccc30d5
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-16 17:06:51 -0300

    Refatoraçao

--- Exercicios/JavaScript/lib/Exercicio3.js
+++ Exercicios/JavaScript/lib/Exercicio3.js
@@ -1,6 +1,9 @@
-function soma(x, y) {
-    return Number(x) + Number(y)
-}
+// function soma(x, y) {
+//     return Number(x) + Number(y)
+// }
+import { soma } from "../biblio.js"
+
+
 console.log(soma(4, 5))
 console.log(soma(10, 15))
 console.log(soma("-3", "7"))
