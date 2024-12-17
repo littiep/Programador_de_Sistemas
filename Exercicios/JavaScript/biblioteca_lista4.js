7561ec036e615316e1a679bcceae3b2c37a48138
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-17 15:19:55 -0300

    add func biblioteca_lista4

--- Exercicios/JavaScript/biblioteca_lista4.js
+++ Exercicios/JavaScript/biblioteca_lista4.js
@@ -6,3 +6,10 @@ export function somarArr(arr) {
     let res = arr.map(Number).reduce(soma, 0)
     return res
 }
+
+
+
+export function mediaLista(arr1) {
+    let media = arr1.map(Number).reduce(soma / arr1.length, 0)
+    return media
+    }
