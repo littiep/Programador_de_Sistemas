4f2f749bda07e9839e2401e8be2f2756efce6d52
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-17 15:20:46 -0300

    a

--- Exercicios/JavaScript/lib/Exercicio5.js
+++ Exercicios/JavaScript/lib/Exercicio5.js
@@ -1,11 +1,11 @@
-// function media(x, y, z) {
-//     return (Number(x) + Number(y) + Number(z)) / 3
-// }
+function media(x, y, z) {
+    return (Number(x) + Number(y) + Number(z)) / 3
+}
 
 // import { mediaLista } from "../biblioteca_lista2.js"
 
 
 
-console.log(mediaLista([10, 20, 30]))
-// console.log(mediaLista(5, 15, 25))
-// console.log(mediaLista("2", "2", "2"))
+console.log(mediaLista(10, 20, 30))
+console.log(mediaLista(5, 15, 25))
+console.log(mediaLista("2", "2", "2"))
