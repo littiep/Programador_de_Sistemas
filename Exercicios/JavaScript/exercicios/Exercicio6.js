4acf1b80cdca787d30372c8b26d33aaba35bed72
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 14:33:06 -0300

    refatorar exer 6

--- Exercicios/JavaScript/exercicios/Exercicio6.js
+++ Exercicios/JavaScript/exercicios/Exercicio6.js
@@ -1,6 +1,8 @@
-function div(x, y) {
-    return x / y
-}
+// function div(x, y) {
+//     return x / y
+// }
+
+import { div } from "../lib/biblioteca_lista2.js"
 console.log(div(10, 3))
 console.log(div(9, 3))
 console.log(div(9, 2))
