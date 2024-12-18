b2c4f2a4b1432d46660259ca6e43cfa1b55f6cac
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 14:34:26 -0300

    exportar/importar exer 13

--- Exercicios/JavaScript/exercicios/Exercicio13.js
+++ Exercicios/JavaScript/exercicios/Exercicio13.js
@@ -1,6 +1,9 @@
-function raizQuadrada(x) { 
-    return x ** 0.5
-}
+// function raizQuadrada(x) { 
+//     return x ** 0.5
+// }
+
+import { raizQuadrada } from "../lib/biblioteca_lista2.js"
+
 console.log(raizQuadrada(16))
 console.log(raizQuadrada(9))
 console.log(raizQuadrada(121))
