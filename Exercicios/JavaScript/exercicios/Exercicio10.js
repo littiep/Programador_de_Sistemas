8fe59133533aeb6fd8e3231eb1ef47a2bbb12de2
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 14:34:04 -0300

    exportar/importar exer 10

--- Exercicios/JavaScript/exercicios/Exercicio10.js
+++ Exercicios/JavaScript/exercicios/Exercicio10.js
@@ -1,6 +1,9 @@
-function mul(a, b) {
-    return a * b 
-}
+// function mul(a, b) {
+//     return a * b 
+// }
+
+import { mul } from "../lib/biblioteca_lista2.js"
+
 console.log(mul(5, 10))
 console.log(mul(7, 3))
 console.log(mul(8, 4))  
