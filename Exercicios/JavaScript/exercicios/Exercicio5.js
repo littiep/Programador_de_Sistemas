255556e51a824967f296aa331a795385fe888dfb
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 14:32:52 -0300

    refatorar exer 6

--- Exercicios/JavaScript/exercicios/Exercicio5.js
+++ Exercicios/JavaScript/exercicios/Exercicio5.js
@@ -1,11 +1,10 @@
-function media(x, y, z) {
-    return (Number(x) + Number(y) + Number(z)) / 3
-}
+// function media(x, y, z) {
+//     return (Number(x) + Number(y) + Number(z)) / 3
+// }
 
-// import { mediaLista } from "../biblioteca_lista2.js"
+import { media } from "../lib/biblioteca_lista2.js"
 
 
-
-console.log(mediaLista(10, 20, 30))
-console.log(mediaLista(5, 15, 25))
-console.log(mediaLista("2", "2", "2"))
+console.log(media(10, 20, 30))
+console.log(media(5, 15, 25))
+console.log(media("2", "2", "2"))
