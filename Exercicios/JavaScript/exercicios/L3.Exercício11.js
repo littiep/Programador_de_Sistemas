8de13ee64628086ed80190e5bf91b3666d5c77aa
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:44:46 -0300

    exporta/importar exer 11

--- Exercicios/JavaScript/exercicios/L3.Exercício11.js
+++ Exercicios/JavaScript/exercicios/L3.Exercício11.js
@@ -1,13 +1,14 @@
-function fatorial(n) {
-    var x = 1, fator = 1
-    while (x <= n ) {
-        // fator = x * n
-        fator *= x
-        x++
+// function fatorial(n) {
+//     var x = 1, fator = 1
+//     while (x <= n ) {
+//         // fator = x * n
+//         fator *= x
+//         x++
         
-    }
-    return fator
-}
+//     }
+//     return fator
+// }
+import { fatorial } from "../lib/biblioteca_lista3.js"
 
 console.log(fatorial(5))
 console.log(fatorial(0))
