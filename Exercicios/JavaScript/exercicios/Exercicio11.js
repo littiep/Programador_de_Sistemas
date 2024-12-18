af238f52a9cba582ffd9a538ac563804554b9901
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 14:34:16 -0300

    exportar/importar exer 11

--- Exercicios/JavaScript/exercicios/Exercicio11.js
+++ Exercicios/JavaScript/exercicios/Exercicio11.js
@@ -1,6 +1,8 @@
-function pot(x, y) {
-    return x ** y
-}
+// function pot(x, y) {
+//     return x ** y
+// }
+
+import { pot } from "../lib/biblioteca_lista2.js"
 console.log(pot(2, 3))
 console.log(pot(5, 2))
 console.log(pot(7, 0))
