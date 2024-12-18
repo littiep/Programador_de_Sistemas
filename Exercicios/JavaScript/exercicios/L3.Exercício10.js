22d4c8db9a843f32139b23f22318de32ffd9e20f
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:44:37 -0300

    exporta/importar exer 10

--- Exercicios/JavaScript/exercicios/L3.Exercício10.js
+++ Exercicios/JavaScript/exercicios/L3.Exercício10.js
@@ -1,11 +1,12 @@
-function contarAteN(n) {
-    var x = 0, soma = 0
-    while (x <= n) {
-        soma += x
-        x++
-    }
-    return soma
-}
+// function contarAteN(n) {
+//     var x = 0, soma = 0
+//     while (x <= n) {
+//         soma += x
+//         x++
+//     }
+//     return soma
+// }
+import { contarAteN } from "../lib/biblioteca_lista3.js"
 
 console.log(contarAteN(5))
 console.log(contarAteN(10))
