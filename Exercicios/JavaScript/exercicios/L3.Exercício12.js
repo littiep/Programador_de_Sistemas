c7344521d9dda9f2df19f59b1b9297fc5e565f3e
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:44:53 -0300

    exporta/importar exer 12

--- Exercicios/JavaScript/exercicios/L3.Exercício12.js
+++ Exercicios/JavaScript/exercicios/L3.Exercício12.js
@@ -11,16 +11,17 @@
 // imprimirTabuada(2)
 // imprimirTabuada(10)
 
-function tabuada(x) {
-    for (var m = 0; m <= 10; m++) {
-        var p = x * m
-        console.log(`${x} x ${m} = ${p}`)
-    }
-    console.log("===========")
-}
+// function tabuada(x) {
+//     for (var m = 0; m <= 10; m++) {
+//         var p = x * m
+//         console.log(`${x} x ${m} = ${p}`)
+//     }
+//     console.log("===========")
+// }
 
 // import { tabuada } from "../biblio.js"
 
+import { tabuada } from "../lib/biblioteca_lista3.js"
 
 tabuada(2)
 tabuada(10)
