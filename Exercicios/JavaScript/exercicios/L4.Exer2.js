79edd777e94869e0c7b9c740965939dc6d684629
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:45:31 -0300

    exporta/importar lista 4 exer  2

--- Exercicios/JavaScript/exercicios/L4.Exer2.js
+++ Exercicios/JavaScript/exercicios/L4.Exer2.js
@@ -7,3 +7,13 @@ console.log(array.indexOf(3))
 console.log(array1.indexOf(20))
 console.log(array2.indexOf(25))
 console.log(array3.indexOf(30))
+
+// function verIndex(arr, pr) {
+//     let array1 = arr.indexOf(pr)
+//     return array1
+// }
+
+// console.log(verIndex([1, 2, 3, 4, 5], 3))
+// console.log(verIndex([10, 20, 30, 40, 50], 20))
+// console.log(verIndex([5, 10, 15, 20, 25], 25))
+// console.log(verIndex([5, 10, 15, 20, 25], 30))
