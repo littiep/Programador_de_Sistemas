d5d25a2cf83d0ccd8030c4ed20f62a3229b9745b
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:43:28 -0300

    exporta/importar exer 3

--- Exercicios/JavaScript/exercicios/L3.Exercício3.js
+++ Exercicios/JavaScript/exercicios/L3.Exercício3.js
@@ -1,13 +1,14 @@
-function positivo(num) {
-     if (num > 0) {
-        return "Positivo"
-    }
-    else if (num < 0) {
-        return "Negativo"
-    }
-        return "Zero"  
-}
+// function positivo(num) {
+//      if (num > 0) {
+//         return "Positivo"
+//     }
+//     else if (num < 0) {
+//         return "Negativo"
+//     }
+//         return "Zero"  
+// }
 
+import { positivo } from "../lib/biblioteca_lista3.js"
 console.log(positivo(5))
 console.log(positivo(-3))
 console.log(positivo(0))
