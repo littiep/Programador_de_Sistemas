d911677192664ad3c747dd051b257848a089825d
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:45:03 -0300

    exporta/importar exer 13

--- Exercicios/JavaScript/exercicios/L3.Exercício13.js
+++ Exercicios/JavaScript/exercicios/L3.Exercício13.js
@@ -1,28 +1,11 @@
-function imprimirTabuada() {
-    for (var n = 1; n <= 10; n++) {
-            for (var k = 0; k <= 10; k++) {
-                    console.log(`${n} x ${k} = ${n*k}`)
-                }    
-                console.log("\n===============\n")
-            }
-}
-// import { imprimirTabuada } from "../biblio.js";
-// imprimirTabuada()
+// function imprimirTabuada() {
+//     for (var n = 1; n <= 10; n++) {
+//             for (var k = 0; k <= 10; k++) {
+//                     console.log(`${n} x ${k} = ${n*k}`)
+//                 }    
+//                 console.log("\n===============\n")
+//             }
+// }
 
-
-// import biblioteca from "../biblio.js"
-// console.log(biblioteca.imprimirTabuada())
-
-
-        
-        // function tabuada(n){
-        //     for(var k=0; k<- 10; k++) {
-        //         console.log(` ${n} x ${k} = ${n*k}`)
-        //     }
-        //     console.log();
-
-        // }
-        // function tabuadaCompleta() {
-        //     for(var n=1; n<=10; n++) tabuada(n); 
-        //  }
-        // tabuadaCompleta();
+import { imprimirTabuada } from "../lib/biblioteca_lista3.js";
+imprimirTabuada()
