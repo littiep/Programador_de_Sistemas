0f5975bed3b4a88061a2c96c0a5cd94d5038a98a
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:47:11 -0300

    exporta/importar lista 4 exer 18

--- Exercicios/JavaScript/exercicios/L4.Exer18.js
+++ Exercicios/JavaScript/exercicios/L4.Exer18.js
@@ -1,13 +1,13 @@
 
-function vogal(x) {
-    var vogais = "aeiou".split('')
-    let k = 0
-    for (i = 0; i < x.length; i++) {
-        if (x.includes(vogais[i])) {
-            k++
-        }
-    }
-    return k
-}
-
+// function vogal(x) {
+//     var vogais = "aeiou".split('')
+//     let k = 0
+//     for (i = 0; i < x.length; i++) {
+//         if (x.includes(vogais[i])) {
+//             k++
+//         }
+//     }
+//     return k
+// }
+import { vogal } from "../lib/biblioteca_lista4.js"
 console.log(vogal("hello"))
