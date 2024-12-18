6fcc4bae6894af92c1e9d3d37ebd28793b34da25
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:44:23 -0300

    exporta/importar exer 9

--- Exercicios/JavaScript/exercicios/L3.Exercício9.js
+++ Exercicios/JavaScript/exercicios/L3.Exercício9.js
@@ -1,21 +1,23 @@
-function sa(nota) {
-   if (nota >= 90 && nota <= 100) {
-       return "A"
-    }
-   else if (nota >= 80 && nota <= 89) {
-       return "B"
-   }
-   else if (nota >= 70 && nota <= 79) {
-       return "C"
-   }
-   else if (nota >= 60 && nota <= 69 ) {
-       return "D"
-   }
-   else if (nota < 60 && nota >= 0){
-       return "F"
-   }
-   return "Nota invalida"
-}
+// function sa(nota) {
+//    if (nota >= 90 && nota <= 100) {
+//        return "A"
+//     }
+//    else if (nota >= 80 && nota <= 89) {
+//        return "B"
+//    }
+//    else if (nota >= 70 && nota <= 79) {
+//        return "C"
+//    }
+//    else if (nota >= 60 && nota <= 69 ) {
+//        return "D"
+//    }
+//    else if (nota < 60 && nota >= 0){
+//        return "F"
+//    }
+//    return "Nota invalida"
+// }
+
+import { sa } from "../lib/biblioteca_lista3.js"
 
 console.log(sa(90))
 console.log(sa(75))
