af30c4d55913f951f231851f82a6ee73cd276df8
Gustavo Gabriel <gusatvo.martinsr@gmail.com>
2024-12-18 16:44:12 -0300

    exporta/importar exer 8

--- Exercicios/JavaScript/exercicios/L3.Exercício8.js
+++ Exercicios/JavaScript/exercicios/L3.Exercício8.js
@@ -1,9 +1,12 @@
-function vogal(letra) {
-   if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
-      return "Vogal"
-   }
-   return "Consoante"
-}
+// function vogal(letra) {
+//    if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
+//       return "Vogal"
+//    }
+//    return "Consoante"
+// }
+ import { vogal } from "../lib/biblioteca_lista3.js"
+
+
    console.log(vogal("a"))
    console.log(vogal("b"))
    console.log(vogal("e"))
